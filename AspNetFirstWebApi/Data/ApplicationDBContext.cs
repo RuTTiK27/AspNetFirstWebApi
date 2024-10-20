00aa08a0661f973c085ac465ce5895f3894b1e92
Rutvik Pandya <rutvikpandya00@gmail.com>
2024-10-20 17:03:23 +0530

    2024 Asp.net WebAPI Practice
    Used DTO and perform CRUD

---
+++ AspNetFirstWebApi/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using AspNetFirstWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetFirstWebApi.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ AspNetFirstWebApi/Dtos/Stock/CreateStockRequestDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspNetFirstWebApi.Dtos.Stock
+{
+    public class CreateStockRequestDto
+    {
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+
+        public string Industry { get; set; } = string.Empty;
+
+        public long MarketCap { get; set; }
+    }
+}
